b9fd912f6e3ca853e83b755ce683b4b296be7cb8
Pedro Henrique <phtotal10@gmail.com>
2023-08-06 20:02:22 -0300

    Estrutura base

--- cardapio/cardapio/src/main/java/com/example/cardapio/controller/FoodController.java
+++ cardapio/cardapio/src/main/java/com/example/cardapio/controller/FoodController.java
@@ -1,15 +1,23 @@
 package com.example.cardapio.controller;
 
-import jakarta.annotation.Resource;
+import com.example.cardapio.food.Food;
+import com.example.cardapio.food.FoodRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping
 public class FoodController {
+    @Autowired
+    private FoodRepository repository;
     @GetMapping
-    public void getAll(){
+    public List<Food> getAll(List<Food> foodList){
 
+      List<Food> food = repository.findAll();
+        return foodList;
     }
 }
